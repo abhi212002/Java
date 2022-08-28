9a7ab66867a2aaaa0b731fad58381fa1c1082971
abhi212002 <65025388+abhi212002@users.noreply.github.com>
2022-08-28 21:48:39 +0530

    Add files via upload

---
+++ Placement_Prep/Two_Sum.java
@@ -0,0 +1,33 @@
+package Placement_Ques;
+
+import java.util.Arrays;
+
+//Given an array A[] and a number x, check for pair in A[] with sum as x
+public class Two_Sum {
+	
+	public static boolean ts(int n, int A[]) {
+		int l=0,r=A.length-1;
+		Arrays.sort(A);
+		while(l<r) {
+			if(A[l]+A[r]==n) {
+				return true;
+			}
+			else if(A[l]+A[r]<n) {
+				l++;
+			}
+			else if(A[l]+A[r]>n) {
+				r--;
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String args[]) {
+		int A[]={1, 4, 45, 6, 10, -8};
+		int n=16;
+		//int n=15;
+		System.out.println(ts(n,A));
+		
+	}
+
+}
